58db6248e877cd506b43d1a2cac41230e8358654
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-09-12 10:57:43 +0700

    refactor: simplify hill climb search

--- src/HillClimbingBot.java
+++ src/HillClimbingBot.java
@@ -1,7 +1,6 @@
 import javafx.scene.control.Button;
 
 public class HillClimbingBot extends Bot {
-
     /**
      * This bot is a hill climbing bot. It will evaluate the current state of the
      * board and make a move that will maximize the score.
@@ -18,23 +17,12 @@ public class HillClimbingBot extends Bot {
             for (int j = 0; j < 8; j++) {
                 // Check if the board is available
                 if (board[i][j].getText().equals("")) {
-                    // Copy the board
-                    Button[][] tempBoard = new Button[8][8];
-                    for (int x = 0; x < 8; x++) {
-                        for (int y = 0; y < 8; y++) {
-                            tempBoard[x][y] = new Button();
-                            tempBoard[x][y].setText(board[x][y].getText());
-                        }
-                    }
-                    board[i][j].setText("O");
-                    board = updateGameBoard(i, j, board, "O");
-                    int score = evaluate(board);
+                    // Calculate the score of the current move
+                    int score = evaluate(board, i, j);
                     if (score > bestScore) {
                         bestScore = score;
                         move = new int[] { i, j };
                     }
-                    // Reset the board
-                    board = tempBoard;
                 }
             }
         }
@@ -43,86 +31,28 @@ public class HillClimbingBot extends Bot {
 
     /**
      * This method evaluates the current state of the board and returns a score
-     * based on the number of X's and O's on the board.
+     * based on the number of adjacent pieces.
      * 
      * @param board The current state of the board.
+     * @param row   The row of the current move.
+     * @param col   The column of the current move.
      * 
      * @return The score of the current state of the board.
      */
-    public int evaluate(Button[][] board) {
-        int playerXScore = 0;
-        int playerOScore = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (board[i][j].getText().equals("X")) {
-                    playerXScore++;
-                } else if (board[i][j].getText().equals("O")) {
-                    playerOScore++;
-                }
-            }
+    public int evaluate(Button[][] board, int row, int col) {
+        int score = 0;
+        if (row - 1 >= 0 && board[row - 1][col].getText().equals("X")) {
+            score++;
         }
-        return playerOScore - playerXScore;
-    }
-
-    /**
-     * Get the score of the player
-     * 
-     * @param board is the game board
-     * @param i     The row number of the button clicked.
-     * @param j     The column number of the button clicked.
-     * 
-     * @return the score of the player
-     *         [0] is the score of the player (X)
-     *         [1] is the score of the bot (O)
-     */
-    private Button[][] updateGameBoard(int i, int j, Button[][] board, String player) {
-        // Value of indices to control the lower/upper bound of rows and columns
-        // in order to change surrounding/adjacent X's and O's only on the game board.
-        // Four boundaries: First & last row and first & last column.
-        int startRow, endRow, startColumn, endColumn;
-
-        if (i - 1 < 0) // If clicked button in first row, no preceding row exists.
-            startRow = i;
-        else // Otherwise, the preceding row exists for adjacency.
-            startRow = i - 1;
-
-        if (i + 1 >= 8) // If clicked button in last row, no subsequent/further row exists.
-            endRow = i;
-        else // Otherwise, the subsequent row exists for adjacency.
-            endRow = i + 1;
-
-        if (j - 1 < 0) // If clicked on first column, lower bound of the column has been reached.
-            startColumn = j;
-        else
-            startColumn = j - 1;
-
-        if (j + 1 >= 8) // If clicked on last column, upper bound of the column has been reached.
-            endColumn = j;
-        else
-            endColumn = j + 1;
-
-        // Search for adjacency for X's and O's or vice versa, and replace them.
-        // Update scores for X's and O's accordingly.
-        for (int x = startRow; x <= endRow; x++) {
-            for (int y = startColumn; y <= endColumn; y++) {
-                // if x and y is diagonal to i and j, skip
-                if (i - 1 == x && j - 1 == y) {
-                    continue;
-                } else if (i - 1 == x && j + 1 == y) {
-                    continue;
-                } else if (i + 1 == x && j - 1 == y) {
-                    continue;
-                } else if (i + 1 == x && j + 1 == y) {
-                    continue;
-                } else {
-                    if (board[x][y].getText().equals("X") && player.equals("O")) {
-                        board[x][y].setText("O");
-                    } else if (board[x][y].getText().equals("O") && player.equals("X")) {
-                        board[x][y].setText("X");
-                    }
-                }
-            }
+        if (row + 1 < 8 && board[row + 1][col].getText().equals("X")) {
+            score++;
         }
-        return board;
+        if (col - 1 >= 0 && board[row][col - 1].getText().equals("X")) {
+            score++;
+        }
+        if (col + 1 < 8 && board[row][col + 1].getText().equals("X")) {
+            score++;
+        }
+        return score;
     }
 }
